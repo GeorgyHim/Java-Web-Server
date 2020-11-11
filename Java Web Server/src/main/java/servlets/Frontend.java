dcc7d13681ff61b891d8ec9f055f8fe536db034f
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-11 22:02:11 +0300

    Write and use Frontend servlet

---
+++ Java Web Server/src/main/java/servlets/Frontend.java
@@ -0,0 +1,23 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    /** Логин пользователя */
+    private String login = "";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
