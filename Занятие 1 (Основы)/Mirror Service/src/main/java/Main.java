d535d521a26c96dd3f3dddacd2fafd071ca857c2
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-13 18:02:16 +0300

    Write Main

---
+++ Занятие 1 (Основы)/Mirror Service/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.MirrorServlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server = createServer(8080);
+        server.start();
+        server.join();
+    }
+
+    private static Server createServer(int port) {
+        MirrorServlet mirrorServlet = new MirrorServlet();
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        handler.addServlet(new ServletHolder(mirrorServlet), "/mirror");
+        Server server = new Server(port);
+        server.setHandler(handler);
+        return server;
+    }
+}
