b5623bb3255f79647b3521e6a65c84266f1bc88c
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-21 18:06:38 +0300

    Add equals to User

--- Занятие 2 (Авторизация)/AuthService/src/main/java/accounts/User.java
+++ Занятие 2 (Авторизация)/AuthService/src/main/java/accounts/User.java
@@ -1,5 +1,7 @@
 package accounts;
 
+import java.util.Objects;
+
 /**
  * Пользователь
  */
@@ -27,5 +29,18 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return login.equals(user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
     }
 }
