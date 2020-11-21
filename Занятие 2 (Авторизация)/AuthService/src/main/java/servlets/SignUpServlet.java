0019a5a235936913275758746ed3bb3fb8425199
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-21 19:45:24 +0300

    Write doPost() for registration

--- Занятие 2 (Авторизация)/AuthService/src/main/java/servlets/SignUpServlet.java
+++ Занятие 2 (Авторизация)/AuthService/src/main/java/servlets/SignUpServlet.java
@@ -1,10 +1,39 @@
 package servlets;
 
 import accounts.AccountService;
+import accounts.User;
+import accounts.UserAlreadyRegistered;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SignUpServlet extends AccountServlet {
 
     public SignUpServlet(AccountService accountService) {
         super(accountService);
+    }
+
+    /**
+     * Метод регистрации пользователя
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        setContentType(response);
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        if (login == null || login.isEmpty() || password == null || password.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        User user = new User(login, password);
+        try {
+            accountService.registerNewUser(user);
+        } catch (UserAlreadyRegistered userAlreadyRegistered) {
+            userAlreadyRegistered.printStackTrace();
+        }
     }
 }
