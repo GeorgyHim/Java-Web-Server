2a0182736b137c1fedc14978672eb575b15df352
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-21 17:15:36 +0300

    Write AccountService

--- Занятие 2 (Авторизация)/AuthService/src/main/java/accounts/AccountService.java
+++ Занятие 2 (Авторизация)/AuthService/src/main/java/accounts/AccountService.java
@@ -19,5 +19,55 @@ public class AccountService {
         this.authorizedUsers = new HashMap<>();
     }
 
+    /**
+     * Метод регистрации нового пользователя
+     *
+     * @param newUser                   - Новый пользователь
+     * @throws UserAlreadyRegistered    - Исключение в ситуации когда указанный логин уже занят
+     */
+    public void registerNewUser(User newUser) throws UserAlreadyRegistered {
+        if (registeredUsers.containsKey(newUser.getLogin()))
+            throw new UserAlreadyRegistered();
+
+        registeredUsers.put(newUser.getLogin(), newUser);
+    }
+
+    /**
+     * Метод получения пользователя по его логину
+     *
+     * @param login - Логин пользователя
+     * @return      - Найденный пользователь либо Null
+     */
+    public User getUserByLogin(String login) {
+        return registeredUsers.get(login);
+    }
+
+    /**
+     * Метод получения авторизованного пользователя по его сессии
+     *
+     * @param sessionId - Идентификатор сессии пользователя
+     * @return          - Найденный пользователь либо Null
+     */
+    public User getAuthorizedUser(String sessionId) {
+        return  authorizedUsers.get(sessionId);
+    }
+
+    /**
+     * Метод авторизации пользователя
+     *
+     * @param sessionId                 - Идентификатор сессии пользвателя
+     * @param user                      - Пользователь
+     * @throws UserAlreadyAuthorized    - Исключение когда указанная сессия уже связана с другим пользователем
+     */
+    public void loginUser(String sessionId, User user) throws UserAlreadyAuthorized {
+        if (authorizedUsers.containsKey(sessionId))
+            throw new UserAlreadyAuthorized();
+
+        authorizedUsers.put(sessionId, user);
+    }
+
+    public void logoutUser(String sessionId) {
+        authorizedUsers.remove(sessionId);
+    }
 
 }
