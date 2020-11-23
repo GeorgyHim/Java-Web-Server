1bd2c8a3c286af8dc4f8b009c07dddf2af47cb15
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-24 01:44:34 +0300

    Refactoring - extract connection with DriverManager into separate method

--- Занятие 3 (Базы Данных)/PostgresDBAccess/src/main/java/Main.java
+++ Занятие 3 (Базы Данных)/PostgresDBAccess/src/main/java/Main.java
@@ -3,28 +3,31 @@ import org.postgresql.ds.PGSimpleDataSource;
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        String query = "SELECT * FROM jc_contact";
+    private static String query = "SELECT * FROM jc_contact";
 
+    static void connectWithDriverManager() {
         try (Connection conn = DriverManager
                 .getConnection("jdbc:postgresql://127.0.0.1:5432/test_db", "postgres", "supergeorgyuser555");
              PreparedStatement preparedStatement = conn.prepareStatement(query) ) {
-
-//            if (conn != null) {
-//                System.out.println("Connected to the database!");
-//            } else {
-//                System.out.println("Failed to make connection!");
-//            }
 
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("first_name"));
             }
-
+            // У меня работает, вывело имена из тестовой таблицы jc_contact
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static void connectWithDataSource() {
+
+    }
+
+    public static void main(String[] args) {
+        connectWithDriverManager();
+
     }
 }
