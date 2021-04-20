bc83d466f1a7f858d1e4448298df1a2547d07ec6
GeorgyHim <georgyhim1997@yandex.ru>
2021-04-20 19:00:14 +0300

    Write ChatWebSocket

---
+++ Занятие 4 (WebSockets)/WebSockets/src/main/java/chat/ChatWebSocket.java
@@ -0,0 +1,31 @@
+package chat;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+
+@WebSocket
+public class ChatWebSocket {
+    private Session session;
+
+    @OnWebSocketConnect
+    private void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnWebSocketMessage
+    private void onMessage(String message) {
+        echo(message);
+    }
+
+    private void echo(String msg) {
+        try {
+            session.getRemote().sendString(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
