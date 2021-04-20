8cd3860eb8ed903f7b77c78642c5ff40c50f2977
GeorgyHim <georgyhim1997@yandex.ru>
2021-04-20 19:09:51 +0300

    Add frontend

--- Занятие 4 (WebSockets)/WebSockets/src/main/java/main/Main.java
+++ Занятие 4 (WebSockets)/WebSockets/src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
 import chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -12,7 +15,14 @@ public class Main {
 
         contextHandler.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
 
-        server.setHandler(contextHandler);
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, contextHandler});
+        server.setHandler(handlers);
+
         server.start();
         System.out.println("Server started");
         server.join();
