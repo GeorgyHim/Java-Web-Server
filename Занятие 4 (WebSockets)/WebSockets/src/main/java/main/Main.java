a71271b79f6e16d83ac13fb3911d921a37d60488
GeorgyHim <georgyhim1997@yandex.ru>
2021-04-20 19:07:26 +0300

    Change port

--- Занятие 4 (WebSockets)/WebSockets/src/main/java/main/Main.java
+++ Занятие 4 (WebSockets)/WebSockets/src/main/java/main/Main.java
@@ -7,7 +7,7 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        Server server = new Server(8010);
         ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
         contextHandler.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
